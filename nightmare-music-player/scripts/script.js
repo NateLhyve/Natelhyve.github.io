385c85c3658b671d452e1f791aeee3ea9d7e87e7
NateLhyve <NateLhyve@gmail.com>
2023-09-07 12:31:12 -0300

    Update script.js

--- nightmare-music-player/scripts/script.js
+++ nightmare-music-player/scripts/script.js
@@ -12,7 +12,7 @@ new Vue({
         {
           name: "Melancholy",
           artist: "xXtha",
-          cover: "",
+          cover: "Natelhyve.github.io/nightmare-music-player/img/1.jpg",
           source: "",
           url: "https://open.spotify.com/intl-pt/track/5lekOdK7bWl5AirX4dvF24?si=63a9371b60174139",
           favorited: false
